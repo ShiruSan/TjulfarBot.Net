7948cd7642c731461b6392ec11df063a7f1772d8
ShiruSan <adrianlechmann6@gmail.com>
2021-08-30 16:17:15 +0200

    started eco command

--- Economy/EcoCommand.cs
+++ Economy/EcoCommand.cs
@@ -1,4 +1,5 @@
-﻿using TjulfarBot.Net.Commands;
+﻿using Discord;
+using TjulfarBot.Net.Commands;
 using TjulfarBot.Net.Managers;
 
 namespace TjulfarBot.Net.Economy
@@ -12,7 +13,21 @@ namespace TjulfarBot.Net.Economy
 
         public override void OnCommand(CommandContext ctx)
         {
-            
+            if (ctx.Arguments.Length == 0)
+            {
+                var builder = new EmbedBuilder();
+                builder.WithTitle("eco Command").WithColor(Color.Blue);
+                builder.WithDescription("Mit diesem Command kannst du dein Taschengeld und Bankkonto abrufen und deine :penny: verwalten.");
+                builder.WithFields(new[]
+                {
+                    new EmbedFieldBuilder().WithName("Nutzung des Commands:").WithValue("`+eco money` Siehe wie viele :penny: du in deiner Tasche hast\n" +
+                        "`+eco money give @Mitglied [Wert]` Gebe anderen Mitgliedern etwas von deinen :penny:\n" +
+                        "`+eco daily` Bekomme täglich vom Bot 100 :penny: geschenkt\n" +
+                        "`+eco bank` Siehe deine :penny: auf deinem Bankkonto\n" +
+                        "`+eco money transfer [Wert]` Zahle :penny: auf dein Bankkonto ein\n" +
+                        "`+eco bank transfer [Wert]` Hebe :penny: von deinem Bankkonto ab")
+                });
+            }
         }
         
     }
